c3693ebe329f19e94ead78fcc8a6c457b750eb81
FranciscoDadone <dadonefran@gmail.com>
2022-09-18 20:36:44 -0300

    Ej7 terminado

--- tp1/ej6.py
+++ tp1/ej6.py
@@ -1,68 +1,55 @@
 import re
 
-"""
-Números enteros pares (ej. 20, -344, -02, 2, 0, 00344).
-"""
-
 
 def validate_a(value: str) -> bool:
+    """
+    Números enteros pares (ej. 20, -344, -02, 2, 0, 00344).
+    """
     return re.search(r"[-+0]?(\d[02468])+$", value) is not None
 
 
-"""
-Números enteros pares sin ceros no significativos (no puede generar -02).
-"""
-
-
 def validate_b(value: str) -> bool:
+    """
+    Números enteros pares sin ceros no significativos (no puede generar -02).
+    """
     return re.search(r"(^[-+]?)([1-9]\d{0,}[02468]$|(^[1-9]+[02468]$|^[2468]$))", value) is not None
 
 
-"""
-Nombre de variables en Python, un identificador comienza con una letra minúscula o un
-guión bajo (_) seguido de cero o más letras minúsculas, guión bajo o dígitos (0 a 9).
-"""
-
-
 def validate_c(value: str) -> bool:
+    """
+    Nombre de variables en Python, un identificador comienza con una letra minúscula o un
+    guión bajo (_) seguido de cero o más letras minúsculas, guión bajo o dígitos (0 a 9).
+    """
     return re.search(r"^[a-z_].*$", value) is not None
 
 
-"""
-{a^nbc^m / n,m ∈| N* ^ n>0 ^ m>0 } , con Σ = {a, b, c}.
-"""
-
-
 def validate_d(value: str) -> bool:
+    """
+    {a^nbc^m / n,m ∈| N* ^ n>0 ^ m>0 } , con Σ = {a, b, c}.
+    """
     return re.search(r"^[a]*[b][c]*$", value) is not None
 
 
-"""
-Direcciones IPv4 (ej. 10.0.1.2, 192.168.1.1, 127.0.0.1, etc.).
-"""
-
-
 def validate_e(value: str) -> bool:
+    """
+    Direcciones IPv4 (ej. 10.0.1.2, 192.168.1.1, 127.0.0.1, etc.).
+    """
     return re.search(r"^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$", value) is not None
 
 
-"""
-Direcciones IPv4 con puerto (ej. 127.0.0.1:8080, 127.0.0.1:4200, 127.0.0.1:80).
-"""
-
-
 def validate_f(value: str) -> bool:
+    """
+    Direcciones IPv4 con puerto (ej. 127.0.0.1:8080, 127.0.0.1:4200, 127.0.0.1:80).
+    """
     return re.search(r"^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?):[1-9]\d*$", value) is not None
 
 
-"""
-Números de tarjeta de crédito Visa (Begin with a 4 and have 13 or 16 digits).
-"""
-
-
 def validate_g(value: str) -> bool:
+    """
+    Números de tarjeta de crédito Visa (Begin with a 4 and have 13 or 16 digits).
+    """
     return re.search(r"[4]\d{12}$|[4]\d{15}$", value) is not None
 
 
-if __name__ == "__main__":
-    print(validate_b("-0222"))
+# if __name__ == "__main__":
+#     print(validate_b("-0222"))
--- tp1/ej7.py
+++ tp1/ej7.py
@@ -1,1 +1,39 @@
 import csv
+import re
+
+
+def convert(input_file_path: str, output_file_path: str) -> None:
+    """
+    Ejercicio 7: Programar una función Python que procese el contenido de un archivo .csv con las
+    columnas [id, first_name, last_name, email, gender, avatar, job_title,
+    language, ssn].
+    En el nuevo archivo de salida .csv:
+    1. se cambiará el orden las columnas first_name y last_name y se elimina gender
+    quedando: [id, last_name, first_name, email, avatar, job_title,
+    language, ssn]
+    2. y se modificara el dominio en las direcciones de correo, sustituyendo el dominio actual
+    (parte derecha luego del símbolo @) por @ing.unlpam.edu.ar
+    :param input_file_path:
+    :param output_file_path:
+    :return:
+    """
+    out = []
+    with open(input_file_path, 'r') as file1:
+        csvreader = csv.reader(file1)
+        for row in csvreader:
+            out.append([
+                row[0],
+                row[2],
+                row[1],
+                re.sub(r"@.*", '@ing.unlpam.edu.ar', row[3]),
+                row[5],
+                row[6],
+                row[7],
+                row[8]])
+    with open(output_file_path, 'w') as file2:
+        csvwriter = csv.writer(file2)
+        csvwriter.writerows(out)
+
+
+# if __name__ == "__main__":
+#     convert("MOCK_DATA.csv", "out.csv")
