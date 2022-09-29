7aeb0efd48c622792fea7a7036ca0e80552ab6f3
Francisco Dadone <dadonefran@gmail.com>
2022-09-29 12:29:34 -0300

    Ejercicios anteriores en jflap del tp2 y arreglos en el python

--- tp2/ej9.py
+++ tp2/ej9.py
@@ -31,36 +31,48 @@ def solve(json_data, trama: str):
     center = 0
     last_print = ""
     last_index = 0
+    error_counter = 0
 
     if len(trama) == 0:
         trama = empty
 
     _print(trama, 0)
-    while q not in end:
+    while True:
+        if q in end:
+            print('OK   ', '(', trama.replace('#', ''), ')')
+            break
         for i in json_data['Delta'][q]:
+            # Detectar cuando la MT se queda trabada
+            error_counter += 1
+            if error_counter > 10000:
+                print('ERROR')
+                return
+
             s = trama[index]
+            if q in end:
+                break
             if json_data['Delta'][q][i]['e'] == s:
                 if "w" in dict(json_data['Delta'][q][i]).keys():
                     trama = trama[:index] + json_data['Delta'][q][i]['w'] + trama[index + 1:]
                 if str(json_data['Delta'][q][i]['m']).lower() == 'r':
                     index += 1
+                    if (index + 1) > len(trama):
+                        trama += empty
                 if str(json_data['Delta'][q][i]['m']).lower() == 'l':
                     index -= 1
+                    #
+                    if (index + 1) == 0:
+                        trama = empty + trama
+                        center += 1
+                        index = 0
                 q = json_data['Delta'][q][i]['q']
-            if (index + 1) == 0:
-                trama = empty + trama
-                center += 1
-                index = 0
-            elif (index + 1) > len(trama):
-                trama += empty
-            if last_print != trama or last_index != index:
-                print('Index: ', index)
-                _print(trama, index)
-                last_index = index
-                last_print = trama
-                time.sleep(1)
-            if q in end:
-                print('OK')
+                if last_print != trama or last_index != index:
+                    _print(trama, index)
+                    last_index = index
+                    last_print = trama
+                    error_counter = 0
+                    time.sleep(1)
+                break
 
 
 def _print(trama, index):
@@ -86,23 +98,27 @@ def parse_jff(file):
         "q0": "",
         "F": []
     }
+
+    id_q = dict() # puede ocurrir que en el xml la id y el nombre sean diferentes
+
     for block in xml['structure']['automaton']['block']:
         if block['@name'] not in j['Q']:
             j['Q'].append(block['@name'])
+            id_q[block['@id']] = block['@name']
         if 'initial' in block:
             j['q0'] = block['@name']
         if 'final' in block:
             j['F'].append(block['@name'])
 
     for transition in xml['structure']['automaton']['transition']:
-        if not 'q' + transition['from'] in j['Delta']:
-            j['Delta']['q' + transition['from']] = {}
+        if not id_q[transition['from']] in j['Delta']:
+            j['Delta'][id_q[transition['from']]] = {}
         new_key = 0
-        for key in dict(j['Delta']['q' + transition['from']]).keys():
+        for key in dict(j['Delta'][id_q[transition['from']]]).keys():
             if new_key < int(key) + 1:
                 new_key = int(key) + 1
-        j['Delta']['q' + transition['from']][str(new_key)] = {
-            "q": "q" + transition['to'],
+        j['Delta'][id_q[transition['from']]][str(new_key)] = {
+            "q": id_q[transition['to']],
             "e": transition['read'] if transition['read'] is not None else "#",
             "w": transition['write'] if transition['write'] is not None else "#",
             "m": transition['move']
